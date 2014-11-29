fb6e4602ba0019dca8c453b7567a7a3aabc3a52c
bittenbybytes <hele@hele-pc.com>
2014-11-29 11:02:09 +0100

    added neopixel driver in pic18 project

---
+++ USB IR Transmitter/SW/pic18_neopixel.X/main.c
@@ -0,0 +1,203 @@
+/******************************************************************************/
+/* Files to Include                                                           */
+/******************************************************************************/
+
+#if defined(__XC)
+#include <xc.h>        /* XC8 General Include File */
+#elif defined(HI_TECH_C)
+#include <htc.h>       /* HiTech General Include File */
+#elif defined(__18CXX)
+#include <p18cxxx.h>   /* C18 General Include File */
+#endif
+
+#if defined(__XC) || defined(HI_TECH_C)
+
+#include <stdint.h>        /* For uint8_t definition */
+#include <stdbool.h>       /* For true/false definition */
+
+#endif
+
+#include "system.h"        /* System funct/params, like osc/peripheral config */
+#include "user.h"          /* User funct/params, such as InitApp */
+#include "delays.h"
+
+/******************************************************************************/
+/* User Global Variable Declaration                                           */
+/******************************************************************************/
+
+/* i.e. uint8_t <variable_name>; */
+
+/******************************************************************************/
+/* Main Program                                                               */
+/******************************************************************************/
+
+#define LED_TRIS TRISCbits.RC2
+#define LED_PIN PORTCbits.RC2
+
+const int clk = 48000000;
+
+#define DELAY_NS(ns) (_delay(ns/83))
+
+#define T0H 400
+#define T1H 1100
+#define T0L 2000
+#define T1L 1300
+
+//#define T0H 000
+//#define T1H 100
+//#define T0L 100
+//#define T1L 000
+
+#define DELAY_H(x) ((x)? _delay(7) : _delay(1))
+#define DELAY_L(x) ((x)? _delay(1) : _delay(7))
+
+sendbyte(uint8_t in)
+{
+	LED_PIN = 1;
+	DELAY_H(in & 1 << 7);
+	LED_PIN = 0;
+	DELAY_L(in & 1 << 7);
+
+	LED_PIN = 1;
+	DELAY_H(in & 1 << 6);
+	LED_PIN = 0;
+	DELAY_L(in & 1 << 6);
+
+	LED_PIN = 1;
+	DELAY_H(in & 1 << 5);
+	LED_PIN = 0;
+	DELAY_L(in & 1 << 5);
+
+	LED_PIN = 1;
+	DELAY_H(in & 1 << 4);
+	LED_PIN = 0;
+	DELAY_L(in & 1 << 4);
+
+
+	LED_PIN = 1;
+	DELAY_H(in & 1 << 3);
+	LED_PIN = 0;
+	DELAY_L(in & 1 << 3);
+
+	LED_PIN = 1;
+	DELAY_H(in & 1 << 2);
+	LED_PIN = 0;
+	DELAY_L(in & 1 << 2);
+
+	LED_PIN = 1;
+	DELAY_H(in & 1 << 1);
+	LED_PIN = 0;
+	DELAY_L(in & 1 << 1);
+
+	LED_PIN = 1;
+	DELAY_H(in & 1 << 0);
+	LED_PIN = 0;
+	DELAY_L(in & 1 << 0);
+
+
+}
+
+void sendRGB(uint8_t rgb[3])
+{
+	LED_TRIS = 0;
+	sendbyte(rgb[1]);
+	sendbyte(rgb[0]);
+	sendbyte(rgb[2]);
+}
+
+uint8_t strip[60*3];
+
+void delay_ms(uint32_t delay)
+{
+	for(uint32_t i = 0; i < delay; i++)
+		Delay100TCYx((clk/4)/100000);
+}
+
+void main(void)
+{
+	/* Configure the oscillator for the device */
+	ConfigureOscillator();
+
+	/* Initialize I/O and Peripherals for application */
+	InitApp();
+
+	/* TODO <INSERT
+	 * USER APPLICATION CODE HERE> */
+
+	TRISBbits.TRISB4 = 0;
+	ADCON1bits.PCFG = 0x0;
+
+	LED_TRIS = 0;
+	LED_PIN = 0;
+	unsigned led = 0;
+	//LED Show
+	LED_PIN = 0;
+	Delay10KTCYx(200);
+
+//	for (int i = 0; i < 30; i++)
+//	{
+//		strip[i * 3] = (i * 4);
+//		strip[i * 3 +1] = (i * 16);
+//		strip[i * 3 +2] = ((30 - i)*8);
+//	}
+//
+//	for (int i = 0; i < 30; i++)
+//	{
+//		strip[(i+30)*3] = ((i + 30)*4);
+//		strip[(i+30)*3 +1] = (i * 16);
+//		strip[(i+30)*3 +2] = (i * 8);
+//	}
+
+	for (int i = 0; i < 60*3; i++)
+	{
+		uint16_t val = (i/3)*4;
+		val *= val;
+		strip[i] = val >> 8;
+	}
+		
+
+//	for(int i = 0; i < 3; i++)
+//	{
+//		strip[i*3+0] = (255*i/3);
+//		strip[i*3+1] = (255*i/3);
+//		strip[i*3+2] = (255*i/3);
+//
+//		strip[i*3+0+3] = (255*(3-i)/3);
+//		strip[i*3+1+3] = (255*(3-i)/3);
+//		strip[i*3+2+3] = (255*(3-i)/3);
+//	}
+
+	while (1)
+	{
+
+		for(int i = 0; i < 60; i++)
+		{
+			sendRGB(&strip[i*3]);
+		}
+
+		//LED Show
+		LED_PIN = 0;
+
+		delay_ms(100);
+
+		led = !(led);
+		PORTBbits.RB4 = led;
+
+		uint8_t tmp[3];		
+		for(int i = 0; i < 3; i++)
+			tmp[i] = strip[59*3 +i];
+
+		for(int i = 59; i >= 0; --i)
+		{
+			strip[i * 3 +3] = strip[i * 3];
+			strip[i * 3 +1+3] = strip[i * 3 +1];
+			strip[i * 3 +2+3] = strip[i * 3 +2];
+		}
+
+		for(int i = 0; i < 3; i++)
+			strip[i] = tmp[i];
+	}
+
+
+}
+
